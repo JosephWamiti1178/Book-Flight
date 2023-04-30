49232667ac008947d184b4134db6a724f74e13f6
josephwamiti <wamiti8711@gmail.com>
2023-04-30 14:26:15 +0300

    getshowflightsusersairports

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,2 +1,12 @@
 class AirportsController < ApplicationController
+    def index
+        airport = Airport.all
+        render json: airport, status: :ok
+    end
+
+    def show
+        airport = Airport.find(params[:id])
+        render json: airport, status: :ok
+    end
+      
 end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,12 @@
 class FlightsController < ApplicationController
+    def index
+        flight = Flight.all
+        render json: flight, status: :ok
+    end
+
+    def show
+        flight = Flight.find(params[:id])
+        render json: flight, status: :ok
+    end
+      
 end
